816fa090b1d71b7a98a7e9639fa029e335b8a02a
Matheus Bento <matheus.th.bento@gmail.com>
2024-01-12 18:37:39 -0300

    Search selected text on jotoba

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.contextMenus.create({
-    id: "jisho-search",
-    title: "Search on Jisho",
+    id: "jotoba-search",
+    title: "Search on Jotoba",
     contexts: ["selection"]
 });
 
 browser.contextMenus.onClicked.addListener(function(info, tab) {
-    if(info.menuItemId == "jisho-search") {
-        searchJisho(info.selectionText);
+    if(info.menuItemId == "jotoba-search") {
+        searchJotoba(info.selectionText);
     }
 });
 
-function searchJisho(data) {
-    browser.tabs.create({url:'http://jisho.org/search/'+data});
+function searchJotoba(data) {
+    browser.tabs.create({url:'https://jotoba.de/search/0/'+data});
 }
